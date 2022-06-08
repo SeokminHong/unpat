756e5c020dee36d16eaaa9ef3beb28249045e7ef
Seokmin Hong <ghdtjrald240@gmail.com>
2022-06-08 16:57:47 +0900

    Fix names

--- tests/macro.rs
+++ tests/macro.rs
@@ -1,97 +1,97 @@
 use unpat::{try_unpat, unpat};
 
 #[derive(PartialEq, Debug)]
-struct TupleStruct(i32, i32);
+struct TestTuple(i32, i32);
 
 #[derive(PartialEq, Debug)]
 struct TestStruct {
     int: i32,
-    tuple_struct: TupleStruct,
+    tuple: TestTuple,
 }
 
 #[allow(dead_code)]
 #[derive(PartialEq, Debug)]
-enum Test {
+enum TestEnum {
     Int(i32),
     Tuple(i32, f64),
-    TupleStruct(TupleStruct),
+    TupleStruct(TestTuple),
     Named { x: i32, y: f64 },
-    TestStruct(TestStruct),
+    Struct(TestStruct),
 }
 
 #[test]
 fn match_enum() {
-    let int = Test::Int(1);
-    unpat!(Test::Int(v) <- int);
+    let int = TestEnum::Int(1);
+    unpat!(TestEnum::Int(v) <- int); // `v = 3` binding is created here.
     assert_eq!(v, 1);
 }
 
 #[test]
 fn match_tuple_enum() {
-    let tup = Test::Tuple(1, 3.5);
-    unpat!(Test::Tuple(a, b) <- tup);
+    let tup = TestEnum::Tuple(1, 3.5);
+    unpat!(TestEnum::Tuple(a, b) <- tup);
     assert_eq!((a, b), (1, 3.5));
 }
 
 #[test]
 fn match_tuple_struct_enum() {
-    let tup_struct = Test::TupleStruct(TupleStruct(1, 2));
-    unpat!(Test::TupleStruct(TupleStruct(a, _)) <- tup_struct);
+    let tup_struct = TestEnum::TupleStruct(TestTuple(1, 2));
+    unpat!(TestEnum::TupleStruct(TestTuple(a, _)) <- tup_struct);
     assert_eq!(a, 1);
 }
 
 #[test]
 fn match_named_enum() {
-    let named = Test::Named { x: 3, y: 5.1 };
+    let named = TestEnum::Named { x: 3, y: 5.1 };
     {
-        unpat!(Test::Named{x, y} <- named);
+        unpat!(TestEnum::Named{x, y} <- named);
         assert_eq!((x, y), (3, 5.1));
     }
     {
-        unpat!(Test::Named{x: a, y} <- named);
+        unpat!(TestEnum::Named{x: a, y} <- named);
         assert_eq!((a, y), (3, 5.1));
     }
     {
-        unpat!(Test::Named{y, ..} <- named);
+        unpat!(TestEnum::Named{y, ..} <- named);
         assert_eq!(y, 5.1);
     }
 }
 
 #[test]
 fn match_struct_enum() {
-    let test_struct = Test::TestStruct(TestStruct {
+    let test_struct = TestEnum::Struct(TestStruct {
         int: 1,
-        tuple_struct: TupleStruct(2, 3),
+        tuple: TestTuple(2, 3),
     });
     {
         unpat!(
-            Test::TestStruct(
-                TestStruct { int, tuple_struct: TupleStruct(x, y) }
+            TestEnum::Struct(
+                TestStruct { int, tuple: TestTuple(x, y) }
             ) <- test_struct
         );
         assert_eq!((int, x, y), (1, 2, 3));
     }
     {
         unpat!(
-            Test::TestStruct(
-                TestStruct { int, tuple_struct: v @ TupleStruct(x, y) }
+            TestEnum::Struct(
+                TestStruct { int, tuple: v @ TestTuple(x, y) }
             ) <- test_struct
         );
         assert_eq!((int, x, y), (1, 2, 3));
-        assert_eq!(v, TupleStruct(2, 3));
+        assert_eq!(v, TestTuple(2, 3));
     }
 }
 
 #[test]
 fn match_mut_enum() {
-    let mut test_struct = Test::TestStruct(TestStruct {
+    let mut test_struct = TestEnum::Struct(TestStruct {
         int: 1,
-        tuple_struct: TupleStruct(2, 3),
+        tuple: TestTuple(2, 3),
     });
 
     unpat!(
-        Test::TestStruct(
-            TestStruct { ref mut int, tuple_struct: TupleStruct(ref mut x, ref mut y) }
+        TestEnum::Struct(
+            TestStruct { ref mut int, tuple: TestTuple(ref mut x, ref mut y) }
         ) <- test_struct
     );
     assert_eq!((*int, *x, *y), (1, 2, 3));
@@ -101,22 +101,22 @@ fn match_mut_enum() {
 fn match_struct() {
     let test_struct = TestStruct {
         int: 1,
-        tuple_struct: TupleStruct(2, 3),
+        tuple: TestTuple(2, 3),
     };
 
     unpat!(
-        TestStruct { int, tuple_struct: TupleStruct(x, y) } <- test_struct
+        TestStruct { int, tuple: TestTuple(x, y) } <- test_struct
     );
     assert_eq!((int, x, y), (1, 2, 3));
 }
 
-fn try_match_tuple(tuple: Test) -> Result<(i32, f64), String> {
-    try_unpat!(Test::Tuple(a, b) <- tuple, String::from(""));
+fn try_match_tuple(tuple: TestEnum) -> Result<(i32, f64), String> {
+    try_unpat!(TestEnum::Tuple(a, b) <- tuple, String::from(""));
     Ok((a, b))
 }
 
 #[test]
 fn try_match_tuple_enum() {
-    assert_eq!((1, 3.5), try_match_tuple(Test::Tuple(1, 3.5)).unwrap());
-    assert!(try_match_tuple(Test::Int(1)).is_err());
+    assert_eq!((1, 3.5), try_match_tuple(TestEnum::Tuple(1, 3.5)).unwrap());
+    assert!(try_match_tuple(TestEnum::Int(1)).is_err());
 }
